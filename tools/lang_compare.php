b42c1efed4122c3f70a3073ed76ecac429f64df8
MBuchalik <dev@martin-buchalik.de>
2022-09-03 16:49:09 +0200

    Format the language comparison tool using Prettier

--- tools/lang_compare.php
+++ tools/lang_compare.php
@@ -4,26 +4,30 @@
  * Compares German/English language files and shows missing phrases in English file
  */
 
-function conv($in)
-{
-	$in = addslashes(htmlentities($in));
-	$in = preg_replace_callback("/([\n]+)/", function($match)
-	{
-		return "' . \"" . str_replace("\n", "\\n", $match[0]) . "\"\n\t\t\t\t. '";
-	}, $in);
-	$in = str_replace("\r", '\\r', $in);
-	return $in;
+function conv($in) {
+    $in = addslashes(htmlentities($in));
+    $in = preg_replace_callback(
+        "/([\n]+)/",
+        function ($match) {
+            return "' . \"" .
+                str_replace("\n", "\\n", $match[0]) .
+                "\"\n\t\t\t\t. '";
+        },
+        $in,
+    );
+    $in = str_replace("\r", '\\r', $in);
+    return $in;
 }
 
-$lang_user = $lang_admin = $lang_client = $lang_custom = array();
-include('../src/serverlib/languages/deutsch.lang.php');
+$lang_user = $lang_admin = $lang_client = $lang_custom = [];
+include '../src/serverlib/languages/deutsch.lang.php';
 $lang_user_de = $lang_user;
 $lang_admin_de = $lang_admin;
 $lang_client_de = $lang_client;
 $lang_custom_de = $lang_custom;
 
-$lang_user = $lang_admin = $lang_client = $lang_custom = array();
-include('../src/serverlib/languages/english.lang.php');
+$lang_user = $lang_admin = $lang_client = $lang_custom = [];
+include '../src/serverlib/languages/english.lang.php';
 $lang_user_en = $lang_user;
 $lang_admin_en = $lang_admin;
 $lang_client_en = $lang_client;
@@ -33,24 +37,32 @@ header('Content-Type: text/html; charset=UTF-8');
 
 echo '<pre>';
 
-foreach($lang_user_de as $key=>$val)
-	if(!isset($lang_user_en[$key]))
-		printf('$lang_user[\'%s\']	= \'%s\';'."\n", $key, conv($val));
+foreach ($lang_user_de as $key => $val) {
+    if (!isset($lang_user_en[$key])) {
+        printf('$lang_user[\'%s\']	= \'%s\';' . "\n", $key, conv($val));
+    }
+}
 echo "\n\n";
 
-foreach($lang_admin_de as $key=>$val)
-	if(!isset($lang_admin_en[$key]))
-		printf('$lang_admin[\'%s\']	= \'%s\';'."\n", $key, conv($val));
+foreach ($lang_admin_de as $key => $val) {
+    if (!isset($lang_admin_en[$key])) {
+        printf('$lang_admin[\'%s\']	= \'%s\';' . "\n", $key, conv($val));
+    }
+}
 echo "\n\n";
 
-foreach($lang_client_de as $key=>$val)
-	if(!isset($lang_client_en[$key]))
-		printf('$lang_client[\'%s\']	= \'%s\';'."\n", $key, conv($val));
+foreach ($lang_client_de as $key => $val) {
+    if (!isset($lang_client_en[$key])) {
+        printf('$lang_client[\'%s\']	= \'%s\';' . "\n", $key, conv($val));
+    }
+}
 echo "\n\n";
 
-foreach($lang_custom_de as $key=>$val)
-	if(!isset($lang_custom_en[$key]))
-		printf('$lang_custom[\'%s\']	= \'%s\';'."\n", $key, conv($val));
+foreach ($lang_custom_de as $key => $val) {
+    if (!isset($lang_custom_en[$key])) {
+        printf('$lang_custom[\'%s\']	= \'%s\';' . "\n", $key, conv($val));
+    }
+}
 echo "\n\n";
 
 echo '</pre>';
