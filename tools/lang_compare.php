3e34d04ccac4897a9f4314aa6dbb0bd5e7ebdc16
MBuchalik <dev@martin-buchalik.de>
2022-09-10 19:42:47 +0200

    Improve the lang comparison tool

--- tools/lang_compare.php
+++ tools/lang_compare.php
@@ -1,68 +1,45 @@
 <?php
-/**
- * lang_compare.php
- * Compares German/English language files and shows missing phrases in English file
- */
+/*
+  Detect language variables that are exit in the German, but not in the English language file. And vice versa.
+  This script should be run from the CLI.
+*/
 
-function conv($in) {
-    $in = addslashes(htmlentities($in));
-    $in = preg_replace_callback(
-        "/([\n]+)/",
-        function ($match) {
-            return "' . \"" .
-                str_replace("\n", "\\n", $match[0]) .
-                "\"\n\t\t\t\t. '";
-        },
-        $in,
-    );
-    $in = str_replace("\r", '\\r', $in);
-    return $in;
-}
+define('SRC_DIR', dirname(dirname(__FILE__)) . '/src/');
 
-$lang_user = $lang_admin = $lang_client = $lang_custom = [];
-include '../src/serverlib/languages/deutsch.lang.php';
+$langArrayNames = ['lang_user', 'lang_admin', 'lang_client', 'lang_custom'];
+
+include SRC_DIR . '/serverlib/languages/deutsch.lang.php';
 $lang_user_de = $lang_user;
 $lang_admin_de = $lang_admin;
 $lang_client_de = $lang_client;
 $lang_custom_de = $lang_custom;
 
+// Reset the language arrays.
+foreach ($langArrayNames as $langArrayName) {
+    $$langArrayName = [];
+}
 $lang_user = $lang_admin = $lang_client = $lang_custom = [];
-include '../src/serverlib/languages/english.lang.php';
+include SRC_DIR . '/serverlib/languages/english.lang.php';
 $lang_user_en = $lang_user;
 $lang_admin_en = $lang_admin;
 $lang_client_en = $lang_client;
 $lang_custom_en = $lang_custom;
 
-header('Content-Type: text/html; charset=UTF-8');
+foreach ($langArrayNames as $langArrayName) {
+    $deVarName = $langArrayName . '_de';
+    $enVarName = $langArrayName . '_en';
 
-echo '<pre>';
+    foreach ($$deVarName as $langKey => $langValue) {
+        if (isset($$enVarName[$langKey])) {
+            continue;
+        }
+        echo "Missing en variable: \${$lang_key}\n";
+    }
 
-foreach ($lang_user_de as $key => $val) {
-    if (!isset($lang_user_en[$key])) {
-        printf('$lang_user[\'%s\']	= \'%s\';' . "\n", $key, conv($val));
+    foreach ($$enVarName as $langKey => $langValue) {
+        if (isset($$deVarName[$langKey])) {
+            continue;
+        }
+        echo "Missing de variable: \${$langArrayName}['{$langKey}']\n";
     }
 }
-echo "\n\n";
-
-foreach ($lang_admin_de as $key => $val) {
-    if (!isset($lang_admin_en[$key])) {
-        printf('$lang_admin[\'%s\']	= \'%s\';' . "\n", $key, conv($val));
-    }
-}
-echo "\n\n";
-
-foreach ($lang_client_de as $key => $val) {
-    if (!isset($lang_client_en[$key])) {
-        printf('$lang_client[\'%s\']	= \'%s\';' . "\n", $key, conv($val));
-    }
-}
-echo "\n\n";
-
-foreach ($lang_custom_de as $key => $val) {
-    if (!isset($lang_custom_en[$key])) {
-        printf('$lang_custom[\'%s\']	= \'%s\';' . "\n", $key, conv($val));
-    }
-}
-echo "\n\n";
-
-echo '</pre>';
